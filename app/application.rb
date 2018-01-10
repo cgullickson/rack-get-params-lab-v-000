375e4badcf0b248e870627113e374a53e8dcdc2a
Chris Gullickson <cgullickson5@gmail.com>
2018-01-10 18:36:08 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -23,6 +23,9 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+    elsif req.path.match(/add/)
+      search_term = req.params["item"]
+      resp.write add_item(search_term)
     else
       resp.write "Path Not Found"
     end
